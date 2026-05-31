403c7014970abbc676cffdd89894bc2a0ae36091
Виталий Еленчук <135379297+vitalii1408@users.noreply.github.com>
2026-05-31 18:06:23 +0300

    task-2

---
+++ js/task-2.js
@@ -0,0 +1,11 @@
+function makeArray(firstArray, secondArray, maxLength) {
+  const combined = [...firstArray, ...secondArray];
+  return combined.length > maxLength ? combined.slice(0, maxLength) : combined;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); 
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); 
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); 
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); 
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); 
