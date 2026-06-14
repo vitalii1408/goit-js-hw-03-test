b942e54540cda7beb0f16a8eed55057bf13ee1ae
Виталий Еленчук <135379297+vitalii1408@users.noreply.github.com>
2026-06-14 11:23:35 +0300

    2

--- js/task-1.js
+++ js/task-1.js
@@ -6,3 +6,4 @@ console.log(slugify("Arrays for beginners"));
 console.log(slugify("English for developer"));
 console.log(slugify("Ten secrets of JavaScript")); 
 console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+
